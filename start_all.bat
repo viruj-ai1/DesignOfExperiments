7ae101ff950814a73d7a6f8262636f57bc65dc0a
viruj-ai1 <projects_ai@virujgroup.com>
2026-09-16 11:46:46 +0530

    Remove unneeded root files

--- start_all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting DOE-1 Backend and Frontend...
-
-start cmd /k "powershell -ExecutionPolicy Bypass -File .\start_backend.ps1"
-start cmd /k "powershell -ExecutionPolicy Bypass -File .\start_frontend.ps1"
-
-echo Both services are starting up!
